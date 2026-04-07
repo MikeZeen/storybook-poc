45925ee1338778f0667c7a9b2a755634b3646815
Mike Ponton <mike.ponton@flaschenpost.de>
2026-04-07 13:49:55 +0200

    update interaction tests for Primary and Secondary variants in PocButton stories

--- src/stories/PocButton.stories.ts
+++ src/stories/PocButton.stories.ts
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from '@storybook/vue3'
 import { expect, fn, userEvent, within } from 'storybook/test'
 import PocButton from '../components/PocButton.vue'
 
+// Hier werden Informationen über die Story definiert die teilweise auch in der App überschreibbar sind (bspw. die argTypes)
 const meta: Meta<typeof PocButton> = {
   title: 'Components/Button',
   component: PocButton,
@@ -21,12 +22,14 @@ export default meta
 
 type Story = StoryObj<typeof PocButton>
 
+// Hier wird die Primary Variante definiert
 export const Primary: Story = {
   args: {
     label: 'Primary Button',
     type: 'primary',
     'onButton-pressed': fn(),
   },
+  // Hier wird der Interaktionstest für die Primary Variante definiert (Button Click -> Ein Emit wird erwartet damit der Test erfolgreich ist)
   play: async ({ canvasElement, args }) => {
     const canvas = within(canvasElement)
     await userEvent.click(canvas.getByRole('button'))
@@ -40,9 +43,10 @@ export const Secondary: Story = {
     type: 'secondary',
     'onButton-pressed': fn(),
   },
+  // Hier wird ein Fehlerhafter Interaktionstest für die Secondary Variante definiert (Button Click -> Zwei Emits werden erwartet damit der Test erfolgreich ist)
   play: async ({ canvasElement, args }) => {
     const canvas = within(canvasElement)
     await userEvent.click(canvas.getByRole('button'))
-    await expect(args['onButton-pressed']).toHaveBeenCalledTimes(1)
+    await expect(args['onButton-pressed']).toHaveBeenCalledTimes(2) // Nutzen 2 damit es fehlschlägt um zu zeigen was dann passiert
   },
 }
